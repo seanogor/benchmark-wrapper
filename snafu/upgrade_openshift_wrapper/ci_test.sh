90c18134ca195579ac8695e31287c9e1e323133b
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2021-04-03 00:30:08 -0400

    Add a CI test for upgrades
    This commit adds a test to be run as part of the CI to make sure the
    upgrade wrapper code is not broken for each of the commits.

--- snafu/upgrade_openshift_wrapper/ci_test.sh
+++ snafu/upgrade_openshift_wrapper/ci_test.sh
@@ -2,4 +2,22 @@
 
 set -x
 
-echo "No CI Test for Cluster Upgrade"
+# Defaults
+timeout=60
+poll_interval=60
+uuid=$(uuidgen)
+
+# Setup benchmark-wrapper
+echo "Installing benchmark-wrapper in python virtual environment"
+if [[ -d "$PWD/upgrade" ]]; then
+  rm -rf upgrade
+fi
+python3 -m venv upgrade
+source upgrade/bin/activate
+pip3 install -e benchmark-wrapper
+
+# Set the upgrade version
+# Upgrade version is same as the current clusterversion to avoid upgrading the CI cluster
+upgrade_version=$(oc get clusterversion -o=jsonpath="{.items[].status.desired.version}")
+echo "Running cluster upgrade to $upgrade_version"
+run_snafu --tool upgrade -u $uuid --version $upgrade_version --timeout $timeout --poll_interval $poll_interval
